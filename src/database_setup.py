e43f41357b469d47f41201599af6f1a28f301766
George-Alex-Berea <alexberea05@gmail.com>
2026-04-11 23:05:53 +0300

    Added lanceDB to eficiently find the elements in the vector and finished the core logic

---
+++ src/database_setup.py
@@ -0,0 +1,44 @@
+import lancedb
+from lancedb.embeddings import get_registry
+from lancedb.pydantic import LanceModel, Vector
+import json
+from typing import Optional
+
+db = lancedb.connect("./company_emb_database")
+emb_model = get_registry().get("sentence-transformers").create(name="all-MiniLM-L6-v2")
+
+class CompanyInfo(LanceModel):
+    name: Optional[str] = None
+    website: Optional[str] = None
+
+class Phrases(LanceModel):
+    text: str = emb_model.SourceField()
+    vector: Vector(emb_model.ndims()) = emb_model.VectorField()
+    data: CompanyInfo
+
+table = db.create_table("company_descriptions", schema=Phrases, mode="overwrite")
+
+def stream_jsonl(filename):
+    with open(filename, "r") as f:
+        for line in f:
+            raw_json = line.strip()
+            parsed_json = json.loads(raw_json)
+            if raw_json:
+                yield {
+                "text": raw_json, 
+                "data": {
+                    "name": parsed_json.get("operational_name"), 
+                    "website": parsed_json.get("website")
+                }
+            }
+
+print("\n--- Populating Database ---")
+print("Could take a moment depending on the size of the dataset and your machine's capabilities...")
+
+table.add(list(stream_jsonl("../companies.jsonl")))
+print("Data added to the database.")
+
+print("Creating index for cosine similarity search...")
+table.create_index(metric="cosine")
+
+print("\n--- Database Setup Complete ---")
---
+++ src/solution.py
@@ -0,0 +1,78 @@
+import lancedb
+from google import genai
+import json
+
+with open(".api_key.txt", "r") as f:
+    api_key = f.read().strip()
+client = genai.Client(api_key=api_key)
+
+db = lancedb.connect("./company_emb_database")
+table = db.open_table("company_descriptions")
+
+print("\n--- Company Matcher Ready ---")
+while True:
+    user_query = input("\nDescribe the kind of company you are looking for (or 'exit'): ")
+    if user_query.lower() == 'exit':
+        break
+
+    prompt = f"""
+    Write an entry describing a hypothetical company which would provide the necessary products or services 
+    for a user searching for: {user_query}. 
+
+    Only provide the entry, no other text. The entry MUST be valid JSON formatted exactly like this:
+    {{
+        "website": "",
+        "operational_name": "",
+        "year_founded": 2026,
+        "address": "",
+        "employee_count": null,
+        "revenue": 0.0,
+        "primary_naics": "",
+        "description": "",
+        "business_model": [],
+        "target_markets": [],
+        "core_offerings": [],
+        "is_public": boolean,
+        "secondary_naics": ""
+    }}
+
+    The description field should be a concise summary of the company and its target market.
+
+    The business_model field should be a list of strings describing the company's business model 
+    (e.g. "B2B", "SaaS", "Subscription", etc.).
+
+    The target_markets field should be a list of strings describing the company's target markets 
+    (e.g. "Small Businesses", "Healthcare", "E-commerce", etc.).
+
+    The core_offerings field should be a list of strings describing the company's core products or services 
+    (e.g. "Cloud Storage", "AI Consulting", "E-commerce Platform", etc.).
+
+    Other fields should be filled in with plausible values based on the description, 
+    or be left uncompletted if there is no plausible value.
+    """
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents=prompt
+    )
+    
+    target_description = response.text.strip()
+    print(f"\nPowerfull LLM description:\n{target_description}")
+
+    results = (
+        table.search(target_description)
+        .metric("cosine")
+        .limit(50)
+        .to_pandas()
+    )
+
+    print(f"\n--- Top 25 Matches Found ---")
+    for i, row in results.iterrows():
+        company_info = row['data']
+        
+        name = company_info.get('name', 'N/A')
+        website = company_info.get('website', 'N/A')
+        
+        score = 1 - row['_distance']
+        
+        print(f"{i+1}. Score: [{score:.4f}] Name: {name} | Website: {website}")
